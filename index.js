c585f991817b2e8a89ce33584e5187ed62348fc6
Doriano <doriano.bergamin18@gmail.com>
2024-02-14 11:31:30 +1100

    static

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 class Formatter {
-  //add static methods here
+  static capitalize(inputString) {
+    return inputString.charAt(0).toUpperCase() + inputString.slice(1);
+  }
+  static sanitize(inputString) {
+    return inputString.replace(/[^a-zA-Z0-9-' ]/g, "");
+  }
+  static titleize(inputString) {
+    const exceptions = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+
+    const titleizedWords = inputString.split(" ").map((word, index) => {
+      if (index === 0 || !exceptions.includes(word.toLowerCase())) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      } else {
+        return word.toLowerCase();
+      }
+    });
+
+    return titleizedWords.join(" ");
+  }
 }
